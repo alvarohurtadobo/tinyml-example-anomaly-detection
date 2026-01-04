a4712ef88e5af5c4bbd8efaa6c5244c75581d63a
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2026-01-05 00:00:16 +0100

    feat: updated deploy esp32 file

--- mahalanobis_distance/esp32_deploy_md/esp32_deploy_md.ino
+++ mahalanobis_distance/esp32_deploy_md/esp32_deploy_md.ino
@@ -1,15 +1,15 @@
 /**
- * Use Mahalanobis Distance on real accelerometer data to see if it's an anomaly
+ * Use Mahalanobis Distance on real temperature data (MAX6675) to see if it's an anomaly
  * 
  * Author: Shawn Hymel
  * Date: May 6, 2020
+ * Modified: Adapted for MAX6675 temperature sensor
  * 
  * License: Beerware
  */
 
 // Library includes
-#include <Adafruit_MSA301.h>
-#include <Adafruit_Sensor.h>
+#include <SPI.h>
 
 // Local includes
 #include "md_model-moving.h"
@@ -21,19 +21,62 @@ extern "C" {
 // Set to 1 to output debug info to Serial, 0 otherwise
 #define DEBUG 1
 
-// Pins
+// Pins for MAX6675
+constexpr int TEMP_CS_PIN = 5;        // Chip Select pin for MAX6675
+constexpr int TEMP_SCK_PIN = 18;      // Clock pin for MAX6675 (SPI SCK)
+constexpr int TEMP_SO_PIN = 19;       // Serial Out pin for MAX6675 (SPI MISO)
 constexpr int BUZZER_PIN = A1;
 
 // Settings
-constexpr int NUM_AXES = 3;           // Number of axes on accelerometer
-constexpr int MAX_MEASUREMENTS = 128; // Number of samples to keep in each axis
+constexpr int NUM_AXES = 1;           // Temperature sensor (1D data)
+constexpr int MAX_MEASUREMENTS = 128; // Number of samples to keep
 constexpr float MAD_SCALE = 1.4826;   // Scale MAD to be inline with SciPy calcs
 constexpr float THRESHOLD = 1.0;     // Any MD over this is an anomaly
 constexpr int WAIT_TIME = 1000;       // ms between sample sets
 constexpr int SAMPLE_RATE = 200;      // How fast to collect measurements (Hz)
 
-// Globals
-Adafruit_MSA301 msa;
+/*******************************************************************************
+ * Functions
+ */
+
+// Read temperature from MAX6675 sensor
+float readMAX6675() {
+  uint16_t data = 0;
+  
+  // Pull CS low to start communication
+  digitalWrite(TEMP_CS_PIN, LOW);
+  delayMicroseconds(1); // Small delay for stability
+  
+  // Read 16 bits
+  for (int i = 15; i >= 0; i--) {
+    digitalWrite(TEMP_SCK_PIN, LOW);
+    delayMicroseconds(1);
+    
+    if (digitalRead(TEMP_SO_PIN)) {
+      data |= (1 << i);
+    }
+    
+    digitalWrite(TEMP_SCK_PIN, HIGH);
+    delayMicroseconds(1);
+  }
+  
+  // Pull CS high to end communication
+  digitalWrite(TEMP_CS_PIN, HIGH);
+  
+  // Check for open circuit (bit 1)
+  if (data & 0x04) {
+#if DEBUG
+    Serial.println("MAX6675: Open circuit detected!");
+#endif
+    return -1.0; // Error value
+  }
+  
+  // Extract temperature (bits 15-3)
+  // Temperature is in 0.25°C increments
+  float temp = (data >> 3) * 0.25;
+  
+  return temp;
+}
 
 /*******************************************************************************
  * Main
@@ -46,13 +89,17 @@ void setup() {
   while (!Serial);
 #endif
 
-  // Initialize accelerometer
-  if (!msa.begin()) {
+  // Initialize MAX6675 pins
+  pinMode(TEMP_CS_PIN, OUTPUT);
+  pinMode(TEMP_SCK_PIN, OUTPUT);
+  pinMode(TEMP_SO_PIN, INPUT);
+  
+  // Set CS high initially
+  digitalWrite(TEMP_CS_PIN, HIGH);
+  
 #if DEBUG
-    Serial.println("Failed to initialize MSA301");
+  Serial.println("MAX6675 temperature sensor initialized");
 #endif
-    while (1);
-  }
 
   // Configure buzzer pin
   pinMode(BUZZER_PIN, OUTPUT);
